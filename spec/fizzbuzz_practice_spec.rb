72761357f3db609abe51ab83c4371fdb7ef7429b
FrankieBoye <frankie4@btinternet.com>
2019-05-23 22:12:46 +0100

    all tests passing

--- spec/fizzbuzz_practice_spec.rb
+++ spec/fizzbuzz_practice_spec.rb
@@ -1,5 +1,15 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it'returns "Fizz" when passed a number divisble by 3' do
-    expect (fizzbuzz(3)).to eq 'Fizz'
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+
+  it'returns "Buzz" when passed a number divisble by 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+
+  it'returns "Fizzbuzz" when passed a number divisble by 3 and 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
   end
 end
